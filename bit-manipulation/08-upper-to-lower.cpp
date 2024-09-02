1e64fcb6c3bcc424be410912f1010ec8710f69c9
kamaleshbala01 <kamalesravi54@gmail.com>
2024-09-02 09:45:23 +0530

    bit in upper to lower

--- bit-manipulation/08-upper-to-lower.cpp
+++ bit-manipulation/08-upper-to-lower.cpp
@@ -1,15 +1,17 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main(){
     char ch;
     cout<<"ENTER LOWERCASE CHARACTER: ";
     cin>>ch;
-    cout<<(char)(ch-32)<<endl;
+    ch = ch^(1<<5);
+    cout<<ch<<endl;
     char ch1;
     cout<<"ENTER UPPERCASE CHARACTER: ";
     cin>>ch1;
-    cout<<(char)(ch1+32)<<endl;
+    ch1 = ch1|(1<<5);
+    cout<<ch1<<endl;
 
 
     return 0;   
